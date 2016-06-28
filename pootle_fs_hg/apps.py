9df33792034eb7372c81fe5f084f0c306708eeb1
Ryan Northey <ryan@synca.io>
2016-06-28 13:00:13 +0100

    Add pootle_fs_hg.apps

--- pootle_fs_hg/apps.py
+++ pootle_fs_hg/apps.py
@@ -6,5 +6,15 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import importlib
 
-default_app_config = 'pootle_fs_hg.apps.PootleFSHgConfig'
+from django.apps import AppConfig
+
+
+class PootleFSHgConfig(AppConfig):
+
+    name = "pootle_fs_hg"
+    verbose_name = "Pootle Hg"
+
+    def ready(self):
+        importlib.import_module("pootle_fs_hg.providers")
